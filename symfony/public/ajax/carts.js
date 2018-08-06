7df2cbb64ec237c7d785bd409508cbcd1d358bab
Karim <karim.khamiss@gmail.com>
2018-08-07 00:33:49 +0200

    empty cart

--- symfony/public/ajax/carts.js
+++ symfony/public/ajax/carts.js
@@ -62,6 +62,33 @@ $(function () {
         finish();
 
     });
+    $("button.EmptyCart").click(function(){
+        waiting();
+        var cart = $(this).data("cart");
+        var client = $(this).data("client");
+        $.ajax({
+            url : '/client/'+client+'/cart/'+cart+'/empty',
+            type: 'POST',
+            success: function (data) {
+                alert(data);
+                $("#add-cart label.alert").fadeOut();
+                if(data == 1)
+                {
+                    location.reload();
+
+                }
+                else {
+                    location.reload();
+                }
+            },
+            error:function(data){
+                tellme(data);
+                // reload(data);
+            }
+        });
+        finish();
+
+    });
     $('#add-cart').submit(function (e) {
         var button = $('#add-cart button[type="submit"]');
         button_waiting(button);
--- symfony/src/Controller/ClientController.php
+++ symfony/src/Controller/ClientController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Cart;
+use App\Entity\Client;
 use App\Entity\ClientCartProduct;
 use App\Entity\Product;
 use Symfony\Component\HttpFoundation\Response;
@@ -38,6 +39,25 @@ class ClientController extends Controller
             ->find($client_cart_product_id);
         $entity_manager->remove($client_cart_product);
         $entity_manager->flush();
+        return new Response(1);
+    }
+    public function empty_cart($cart_id,$client_id)
+    {
+        $entity_manager = $this->getDoctrine()->getManager();
+        $client = $this->getDoctrine()->getRepository(Client::class)->find($client_id);
+        $cart = $this->getDoctrine()->getRepository(Cart::class)->find($cart_id);
+        $client_cart_product_to_delete_query = $this->getDoctrine()->getRepository(ClientCartProduct::class)
+            ->createQueryBuilder('ccp')
+            ->where('ccp.client = :client')
+            ->andWhere('ccp.cart = :cart')
+            ->setParameters(array('client'=>$client,'cart'=>$cart))
+            ->getQuery();
+        $client_cart_product_to_delete_array = $client_cart_product_to_delete_query->execute();
+        foreach ($client_cart_product_to_delete_array as $client_cart_product_to_delete_item)
+        {
+            $entity_manager->remove($client_cart_product_to_delete_item);
+            $entity_manager->flush();
+        }
         return new Response(1);
     }
 }
