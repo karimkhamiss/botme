2f653420776551d9816a907874fd21c1f6deab10
Karim <karim.khamiss@gmail.com>
2018-08-08 16:03:17 +0200

    Form Base

--- symfony/src/Controller/CategoryController.php
+++ symfony/src/Controller/CategoryController.php
@@ -2,7 +2,9 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -10,11 +12,23 @@ class CategoryController extends AbstractController
 {
     public function add(Request $request)
     {
-       $entity_manager = $this->getDoctrine()->getManager();
-       $category = new Category();
-       $category->setName($request->request->get('name'));
-       $entity_manager->persist($category);
-       $entity_manager->flush();
-       return new Response(1);
+        $category = new Category();
+        $form = $this->createFormBuilder($category)
+            ->add('name', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Add Category'))
+            ->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category = $form->getData();
+            $entity_manager = $this->getDoctrine()->getManager();
+            $entity_manager->persist($category);
+            $entity_manager->flush();
+            return $this->redirectToRoute('categories');
+        }
+        return $this->render('bases/form.html.twig',array(
+            'form' =>$form->createView(),
+            'title'=>"Add Category",
+            'route_to_back'=>"/categories"
+        ));
     }
 }
